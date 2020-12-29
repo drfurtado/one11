55c4fefd198a54a8134185221c7ea6c7ff088f76
drfurtado <ovande.furtado@csun.edu>
2020-12-29 13:54:17 -0800

    First post
    First post added.

---
+++ how-to.R
@@ -0,0 +1,12 @@
+
+#load library
+library(distill)
+
+#Create post
+create_post("Adding a quiz to Canvas Studio", draft = TRUE)
+
+# rename to reflect the title and date in the post YAML front-matter
+rename_post_dir("_posts/2020-12-17-adding-a-quiz-to-canvas-studio")
+
+# Documentation: 
+

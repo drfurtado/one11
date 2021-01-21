39600680e798b6f596881119e604778d263da0bd
drfurtado <ovande.furtado@csun.edu>
2021-01-21 14:55:38 -0800

    several changes

--- how-to.R
+++ how-to.R
@@ -1,9 +1,10 @@
-
-#load library
+# How to create new posts
+# 1 > load library
 library(distill)
 
 #Create post
-create_post("Adding a quiz to Canvas Studio", draft = TRUE)
+create_post("How to phrase results following APA Style?", draft = FALSE)
+
 
 # rename to reflect the title and date in the post YAML front-matter
 rename_post_dir("_posts/2020-12-17-adding-a-quiz-to-canvas-studio")
